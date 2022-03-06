2781f91458ef2694eb66575e774b159ebb2cf1b8
tangtao <15087662661@139.com>
2022-03-06 15:39:46 +0800

    初始化

---
+++ src/main/java/com/hst/HstApplication.java
@@ -0,0 +1,13 @@
+package com.hst;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HstApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HstApplication.class, args);
+    }
+
+}
